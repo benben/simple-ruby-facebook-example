57f718ffa3a1813ccd0bdc1c35dc5dfeee986a73
Benjamin Knofe <ben@nerdlabor.de>
2013-05-26 00:35:14 +0200

    format code: tabs to spaces

--- lib/simple-ruby-facebook-example.rb
+++ lib/simple-ruby-facebook-example.rb
@@ -9,41 +9,41 @@ APP_SECRET = '76dhf8656a75...'
 
 class SimpleRubyFacebookExample < Sinatra::Application
 
-	use Rack::Session::Cookie, secret: 'PUT_A_GOOD_SECRET_IN_HERE'
+  use Rack::Session::Cookie, secret: 'PUT_A_GOOD_SECRET_IN_HERE'
 
-	get '/' do
-		if session['access_token']
-		  'You are logged in! <a href="/logout">Logout</a>'
-			# do some stuff with facebook here
-			# for example:
-			# @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
-			# publish to your wall (if you have the permissions)
-			# @graph.put_wall_post("I'm posting from my new cool app!")
-			# or publish to someone else (if you have the permissions too ;) )
-			# @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
-		else
-			'<a href="/login">Login</a>'
-		end
-	end
+  get '/' do
+    if session['access_token']
+      'You are logged in! <a href="/logout">Logout</a>'
+      # do some stuff with facebook here
+      # for example:
+      # @graph = Koala::Facebook::GraphAPI.new(session["access_token"])
+      # publish to your wall (if you have the permissions)
+      # @graph.put_wall_post("I'm posting from my new cool app!")
+      # or publish to someone else (if you have the permissions too ;) )
+      # @graph.put_wall_post("Checkout my new cool app!", {}, "someoneelse's id")
+    else
+      '<a href="/login">Login</a>'
+    end
+  end
 
-	get '/login' do
-		# generate a new oauth object with your app data and your callback url
-		session['oauth'] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, "#{request.base_url}/callback")
-		# redirect to facebook to get your code
-		redirect session['oauth'].url_for_oauth_code()
-	end
+  get '/login' do
+    # generate a new oauth object with your app data and your callback url
+    session['oauth'] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, "#{request.base_url}/callback")
+    # redirect to facebook to get your code
+    redirect session['oauth'].url_for_oauth_code()
+  end
 
-	get '/logout' do
-		session['oauth'] = nil
-		session['access_token'] = nil
-		redirect '/'
-	end
+  get '/logout' do
+    session['oauth'] = nil
+    session['access_token'] = nil
+    redirect '/'
+  end
 
-	#method to handle the redirect from facebook back to you
-	get '/callback' do
-		#get the access token from facebook with your code
-		session['access_token'] = session['oauth'].get_access_token(params[:code])
-		redirect '/'
-	end
+  #method to handle the redirect from facebook back to you
+  get '/callback' do
+    #get the access token from facebook with your code
+    session['access_token'] = session['oauth'].get_access_token(params[:code])
+    redirect '/'
+  end
 end
 
